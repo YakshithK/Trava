8edfbfa770bc2332acaa32453a46df30a04272df
Yakshith Kommineni <Yakshith.kommineni@gmail.com>
2025-05-25 20:40:10 -0400

    added vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/next"
 import Welcome from "./pages/Welcome";
 import Onboarding from "./pages/Onboarding";
 import TripPosting from "./pages/TripPosting";
@@ -120,6 +120,7 @@ const App = () => (
         </AuthProvider>
       </TooltipProvider>
     </QueryClientProvider>
+    <Analytics />
   </ErrorBoundary>
 );
 
